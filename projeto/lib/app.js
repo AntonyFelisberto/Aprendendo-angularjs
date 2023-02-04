77de0ad2f6df406efda22a04052d781970a16b1b
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-02-04 20:48:36 -0300

    colocando filtragem por inputs

--- projeto/lib/app.js
+++ projeto/lib/app.js
@@ -3,6 +3,7 @@ var app = angular.module("myApp",[]);
 //app.controller("myController",function($scope,$http){ FUNÇÃO TRADICIONAL
 app.controller("myController",["$scope","$http",function($scope,$http){
     $http.get('js/data.json').then(function(response){
-        $scope.artistas = response.data
+        $scope.artistas = response.data;
+        $scope.artistasOrdenacao = 'name';
     })
 }])
