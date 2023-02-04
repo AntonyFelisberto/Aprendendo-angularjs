1d3b05ca4a444761eaa0edec537cd5e04999cf7a
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-02-04 19:00:28 -0300

    adicionando metodo for e http requisições

--- projeto/lib/app.js
+++ projeto/lib/app.js
@@ -1,5 +1,8 @@
 var app = angular.module("myApp",[]);
 
-app.controller("myController",["$scope",function($scope){
-    $scope.mensagem = "OLA MUNDO";
+//app.controller("myController",function($scope,$http){ FUNÇÃO TRADICIONAL
+app.controller("myController",["$scope","$http",function($scope,$http){
+    $http.get('js/data.json').then(function(response){
+        $scope.artistas = response.data
+    })
 }])
